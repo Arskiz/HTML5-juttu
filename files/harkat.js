2d09dd364b0a98ff3eafb57f412598e1ffa5fe27
Arskiz <129842259+Arskiz@users.noreply.github.com>
2023-05-03 12:46:58 +0300

    L

--- files/harkat.js
+++ files/harkat.js
@@ -1,5 +1,7 @@
 let avattu = false;
-
+let Counter = 0;
+let currentNumber = 0;
+let oldPasswords = [];
 //Tehtävä 6
 function demoa(){
     document.getElementById("t6h1").className = 'muutos';
@@ -19,4 +21,35 @@ function NavOpen(){
         avattu = false;
         NavMenu.style.display = "none";
     }
+}
+
+//Responsiivinen sivu
+function MakePassword(){
+    let resultText = document.getElementById("Result");
+    let counter = document.getElementById("Counter");
+    
+    currentNumber = (generateRandomIntegerInRange(0, 1000000000, 3));
+        resultText.textContent = "Tulos: " + currentNumber;
+        oldPasswords.push(currentNumber);
+        currentNumber = 0;
+    Counter += 1;
+    counter.textContent = "Salasanoja generoitu: " + Counter;
+
+
+}
+
+function generateRandomIntegerInRange(min, max, multiplier) {
+    return Math.floor(Math.random() * (max - min + 1 * multiplier)) + min;
+}
+
+function showOldPasswords(){
+    if(oldPasswords.length != 0){
+        console.log("Vanhat Salasanat:")
+        console.log(oldPasswords);
+    }
+    else
+    {
+        console.log("Et ole tehnyt yhtään vielä!")
+    }
+    
 }
